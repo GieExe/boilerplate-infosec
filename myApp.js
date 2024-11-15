4e150735702e327993e44c68e1d51500b48f4286
GieExe <103984696+GieExe@users.noreply.github.com>
2024-11-15 11:14:25 +0800

    ssd
    ds

--- myApp.js
+++ myApp.js
@@ -1,61 +1,16 @@
 const express = require('express');
+const helmet = require('helmet');
+
 const app = express();
 
+// Use Helmet to help secure the app by setting various HTTP headers
+app.use(helmet());
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-module.exports = app;
-const api = require('./server.js');
-app.use(express.static('public'));
-app.disable('strict-transport-security');
-app.use('/_api', api);
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + '/views/index.html');
+app.get('/', (req, res) => {
+  res.send('Hello, Helmet is securing this app!');
 });
-let port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Your app is listening on port ${port}`);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
